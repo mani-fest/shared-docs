a494f7cf247441336a4d803015a3fc44204bb6a9
mani-fest <mtewary09@gmail.com>
2026-09-15 11:34:13 +0530

    Update 2

--- doc-main/docusaurus.config.js
+++ doc-main/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://mani-fest.io',
+  url: 'http://localhost:3000/docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
